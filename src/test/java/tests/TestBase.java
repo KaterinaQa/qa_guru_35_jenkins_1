94af096d9b174b1b5dab997a9567d2786fa970a7
katerinaqa <knedostaeva@sbdagroup.com>
2025-06-01 13:30:19 +0300

    fix test base

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -20,41 +20,40 @@ public class TestBase {
     }
 
 
-        @BeforeAll
-        public static void setup() {
+    @BeforeAll
+    public static void setup() {
 
-            WebDriverManager.chromedriver()
-                    .clearDriverCache()
-                    .clearResolutionCache()
-                    .setup();
+        WebDriverManager.chromedriver()
+                .clearDriverCache()
+                .clearResolutionCache()
+                .setup();
 
 
-            DesiredCapabilities capabilities = new DesiredCapabilities();
-            capabilities.setCapability("selenoid:options", Map.<String, Object>of(
-                    "enableVNC", true,
-                    "enableVideo", true
-            ));
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
 
-            Configuration.browserCapabilities = capabilities;
+        Configuration.browserCapabilities = capabilities;
 
-            Configuration.baseUrl = "https://demoqa.com";
-            Configuration.browserSize = System.getProperty("screenResolution", "1920x1080");
-            Configuration.browser = System.getProperty("browser", "chrome");
-            Configuration.browserVersion = System.getProperty("browserVersion", "127.0");
-            Configuration.pageLoadStrategy = "eager";
-            Configuration.timeout = 10000;
-            Configuration.remote = String.format("https://%s:%s@%s/wd/hub",
-                    System.getProperty("selenoid_login", "user1"),
-                    System.getProperty("selenoid_password", "1234"),
-                    System.getProperty("selenoid_host", "selenoid.autotests.cloud"));
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = System.getProperty("screenResolution", "1920x1080");
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browserVersion = System.getProperty("browserVersion", "127.0");
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.timeout = 10000;
+        Configuration.remote = String.format("https://%s:%s@%s/wd/hub",
+                System.getProperty("selenoid_login", "user1"),
+                System.getProperty("selenoid_password", "1234"),
+                System.getProperty("selenoid_host", "selenoid.autotests.cloud"));
         }
 
-        @AfterEach
-        void addAttachments() {
-            Attach.screenshotAs("Последний скрин");
-            Attach.pageSource();
-            Attach.browserConsoleLogs();
-            Attach.addVideo();
-        }
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Последний скрин");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
     }
 }
