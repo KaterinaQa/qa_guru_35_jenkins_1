2aef5159a5b2f353205dfc413441be7056cad6f2
katerinaqa <knedostaeva@sbdagroup.com>
2025-06-01 12:21:22 +0300

    update config

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -6,55 +6,55 @@ import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 import java.util.Map;
 
 public class TestBase {
-    @BeforeAll
-    public static void setup() {
 
-
-        String selenoidHost = System.getProperty("selenoid_host", "selenoid.autotests.cloud");
-        String selenoidLogin = System.getProperty("selenoid_login", "user1");
-        String selenoidPassword = System.getProperty("selenoid_password", "1234");
-        String browser = System.getProperty("browser", "chrome");
-        String browserVersion = System.getProperty("browserVersion", "127.0");
-        String screenResolution = System.getProperty("screenResolution", "1920x1080");
-
-        WebDriverManager.chromedriver()
-                .clearDriverCache()
-                .clearResolutionCache()
-                .setup();
-
+    @BeforeEach
+    public void beforeEach() {
         SelenideLogger.addListener("allure", new AllureSelenide());
-
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
-                "enableVNC", true,
-                "enableVideo", true
-        ));
-
-        Configuration.browserCapabilities = capabilities;
-
-        Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = screenResolution;
-        Configuration.browser = browser;
-        Configuration.browserVersion = browserVersion;
-        Configuration.pageLoadStrategy = "eager";
-        Configuration.timeout = 10000;
-        Configuration.remote = String.format("https://%s:%s@%s/wd/hub",
-                selenoidLogin,
-                selenoidPassword,
-                selenoidHost);
     }
 
-    @AfterEach
-    void addAttachments() {
-        Attach.screenshotAs("Последний скрин");
-        Attach.pageSource();
-        Attach.browserConsoleLogs();
-        Attach.addVideo();
+
+        @BeforeAll
+        public static void setup() {
+
+            WebDriverManager.chromedriver()
+                    .clearDriverCache()
+                    .clearResolutionCache()
+                    .setup();
+
+
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                    "enableVNC", true,
+                    "enableVideo", true
+            ));
+
+            Configuration.browserCapabilities = capabilities;
+
+            Configuration.baseUrl = "https://demoqa.com";
+            Configuration.browserSize = System.getProperty("screenResolution", "1920x1080");
+            Configuration.browser = System.getProperty("browser", "chrome");
+            Configuration.browserVersion = System.getProperty("browserVersion", "127.0");
+            Configuration.pageLoadStrategy = "eager";
+            Configuration.timeout = 10000;
+            Configuration.remote = String.format("https://%s:%s@%s/wd/hub",
+                    System.getProperty("selenoid_login", "user1"),
+                    System.getProperty("selenoid_password", "1234"),
+                    System.getProperty("selenoid_host", "selenoid.autotests.cloud"));
+        }
+
+        @AfterEach
+        void addAttachments() {
+            Attach.screenshotAs("Последний скрин");
+            Attach.pageSource();
+            Attach.browserConsoleLogs();
+            Attach.addVideo();
+        }
     }
 }
